1348aa7d4b96689725b5f3c2bbf18ff3334ac5aa
Akash Tripathi <akash.tripathi@reverfin.ai>
2026-01-08 17:00:46 +0530

    feat: revamp user profile setting pages UI

--- web/src/app/(withProfileLayout)/profile/page.tsx
+++ web/src/app/(withProfileLayout)/profile/page.tsx
@@ -5,10 +5,6 @@ import { ProfileSettings } from "@rever/common";
 const Profile = () => {
   return (
     <>
-      <p className="text-slate-800 dark:text-slate-100 text-lg font-semibold mb-6">
-        Profile
-      </p>
-
       <ProfileSettings />
     </>
   );
