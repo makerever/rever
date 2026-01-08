7e9c3b824d9ef51b8d2fce4a64d953539f14ee89
Akash Tripathi <akash.tripathi@reverfin.ai>
2026-01-08 17:00:46 +0530

    feat: revamp user profile setting pages UI

--- web/src/app/(withProfileLayout)/security/page.tsx
+++ web/src/app/(withProfileLayout)/security/page.tsx
@@ -5,10 +5,6 @@ import { ChangePassword } from "@rever/common";
 const Security = () => {
   return (
     <>
-      <p className="text-slate-800 dark:text-slate-100 text-lg font-semibold mb-6">
-        Change password
-      </p>
-
       <ChangePassword />
     </>
   );
